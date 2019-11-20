219ee54b660af7dca28acf8c8e6512da162bec7e
Manuel Hernández Padrón <37270235+ManuHP23@users.noreply.github.com>
2019-11-20 17:48:10 +0000

    Eventos

---
+++ Técnicas_de_Desarrollo/GameEvents/Manuel/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState { Idle, Playing, Ended }
+
+public class GameController : MonoBehaviour 
+{
+
+	public float parallaxSpeed = 0.02f;
+	public RawImage background;
+	public RawImage background2;
+	public RawImage platform;
+    public GameObject uiIdle;
+
+	public GameState gameState = GameState.Idle;
+
+    public GameObject player;
+    public GameObject rockGenerator;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+		//Empieza el juego
+		if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)))
+        {
+			gameState = GameState.Playing;
+            uiIdle.SetActive(false);
+            player.SendMessage("UpdateState", "PlayerRun");
+            rockGenerator.SendMessage("StartGenerator");
+        }
+        //Juego en marcha
+        else if (gameState == GameState.Playing)
+        {
+            Parallax();
+        }
+
+        //Juego finalizado
+        else if (gameState == GameState.Ended)
+        {
+            //to do
+        }
+    }
+    void Parallax()
+    {
+         float finalSpeed = parallaxSpeed * Time.deltaTime;
+         background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+         background2.uvRect = new Rect(background2.uvRect.x + finalSpeed, 0f, 1f, 1f);
+         platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+    }
+    
+}
---
+++ Técnicas_de_Desarrollo/GameEvents/Manuel/Scripts/RockGeneratorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockGeneratorController : MonoBehaviour {
+
+    public GameObject rockPrefab;
+    public float generatorTimer = 1.75f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    void CreateRock()
+    {
+        Instantiate(rockPrefab, transform.position, Quaternion.identity);
+    }
+
+    public void StartGenerator()
+    {
+        InvokeRepeating("CreateRock", 0f, generatorTimer);
+    }
+
+    public void CancelGenerator(bool clean = false)
+    {
+        CancelInvoke("CreateRock");
+        if (clean)
+        {
+            object[] allRocks = GameObject.FindGameObjectsWithTag("Rock");
+            foreach (GameObject rock in allRocks)
+            {
+                Destroy(rock);
+            }
+        }
+    }
+}
