c82fc258a75d465b167096ea2cb40bbdabdcef98
Ernesto <ernestoechegon@gmail.com>
2019-12-03 19:27:27 +0000

    Scroll de fondo

---
+++ Técnicas_de_Desarrollo/Técnicas/Scroll de fondo/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] backgrounds;
+    public GameObject[] pull;
+    public List<GameObject> pool;
+    public float scrollSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pool = new List<GameObject>(pull);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject i in backgrounds)
+        {
+            i.transform.Translate(Vector2.down * scrollSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Asteroid"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            collision.gameObject.GetComponent<Transform>().position = new Vector2(23f, -3f);
+            pool.Add(collision.gameObject);
+        } else {
+            collision.GetComponent<Transform>().position = new Vector2(collision.GetComponent<Transform>().position.x, collision.GetComponent<Transform>().position.y + (11.52f * backgrounds.Length));
+            int rnd = Random.Range(1, 5);
+            for (int i = 0; i < rnd; i++)
+            {
+                if (pool.Count > 0)
+                {
+                    int rpll = Random.Range(0, pool.Count);
+                    GameObject asteroide = pool[rpll];
+                    pool.Remove(asteroide);
+                    asteroide.GetComponent<Transform>().position = new Vector2(-7.2f + Random.Range(0f, 7.2f), collision.GetComponent<Transform>().position.y + (11.52f * backgrounds.Length) - 5.76f + Random.Range(0f, 17.28f));
+                    asteroide.GetComponent<Rigidbody2D>().AddForce(Vector2.down * scrollSpeed * Random.Range(40f, 150f));
+                }
+            }
+        }
+    }
+}
