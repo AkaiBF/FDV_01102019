b63506259e67a6b81eb1487e6c026b0d9e9be3a6
GPescadorBarreto <alu0100895605@ull.edu.es>
2019-12-04 17:51:10 +0000

    Actualizacion scripts

--- Técnicas_de_Desarrollo/Cámara/CopiaScripts/GameController.cs
+++ Técnicas_de_Desarrollo/Cámara/CopiaScripts/GameController.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public Camera mainCamera;
     public GameObject camera1;
     public GameObject camera2;
     public GameObject camera3;
-    private GameObject currentCam;
+    private CinemachineBrain cinemaBrain;
     // Awake is called before the game starts
     void Awake()
     {
-      currentCam = camera2;
+        cinemaBrain = mainCamera.GetComponent<CinemachineBrain>();
     }
 
     // Update is called once per frame
@@ -20,29 +22,25 @@ public class GameController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.F1))
         {
             camera1.SetActive(true);
-            camera2.SetActive(false);
-            currentCam = camera1;
         }
         if (Input.GetKeyDown(KeyCode.F2))
         {
             camera2.SetActive(true);
             camera1.SetActive(false);
-            currentCam = camera2;
         }
         if (Input.GetKeyDown(KeyCode.F3))
         {
             camera1.SetActive(false);
             camera2.SetActive(false);
             camera3.SetActive(true);
-            currentCam = camera3;
         }
         if (Input.GetMouseButtonDown(0))
         {
-          currentCam.GetComponent<CamNoise>().Shake(0.1f);
+          getCurrentCam().GetComponent<CamNoise>().Shake(0.1f);
         }
     }
 
     public GameObject getCurrentCam(){
-      return currentCam;
+        return cinemaBrain.ActiveVirtualCamera.VirtualCameraGameObject;
     }
 }
